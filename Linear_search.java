5e97d51d9358b245094b9a07349a78bd6de1c3d8
Amit180745 <thakuramit9019@gmail.com>
2024-02-19 23:53:02 +0530

    Linear_Search

---
+++ Linear_search.java
@@ -0,0 +1,29 @@
+package practice_DSA;
+import java.util.*;
+public class Linear_search {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter the Length of Array: ");
+		int n = s.nextInt();
+		int [] arr = new int[n];
+		for(int i = 0; i<arr.length; i++) {
+			arr[i] = s.nextInt();
+		}
+		
+		int item = s.nextInt();
+		System.out.println(findindex(arr,item));
+
+	}
+	
+	public static int findindex(int [] arr , int item) {
+		for(int i = 0; i<arr.length; i++) {
+			if(arr[i] == item) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+}
