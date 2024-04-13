7666dab261762220432df3cd6bda0a7cf1e8a0a8
Amit180745 <thakuramit9019@gmail.com>
2024-04-13 23:37:24 +0530

    QPs

---
+++ Target_sum_pairs.java
@@ -0,0 +1,34 @@
+package practice_DSA;
+import java.util.*;
+public class Target_sum_pairs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int []arr =  new int[n];
+		int i = 0;
+		while(i<arr.length) {
+			arr[i] = sc.nextInt();
+			i++;
+		}
+		System.out.println();
+		PairSum(arr);
+
+	}
+	public static void PairSum(int []arr) {
+		Scanner sc = new Scanner(System.in);
+		int target = sc.nextInt();
+		int i = 0;
+		int j = arr.length - 1;
+		while(i<j) {
+			int sum = arr[i] + arr[j];
+			if(sum == target) {
+				System.out.print(arr[i] + " and " + arr[j]);
+			}
+			i++;
+			j--;
+		}
+	}
+
+}
