1c508579837f7f04a22d0c1666c26c4ee57e8a60
Amit180745 <thakuramit9019@gmail.com>
2024-04-13 23:36:54 +0530

    QPs

---
+++ Check_Prime.java
@@ -0,0 +1,23 @@
+package practice_DSA;
+
+public class Check_Prime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Isprime(57));
+
+	}
+
+	public static boolean Isprime(int n) {
+		int i = 2;
+		while (i * i <= n) {
+			if (n % i == 0) {
+				return false;
+			}
+			i++;
+		}
+		return true;
+
+	}
+
+}
